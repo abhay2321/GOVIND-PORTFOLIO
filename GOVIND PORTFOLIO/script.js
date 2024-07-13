fc8a4c01d5146337704ec0b390b3f8995f082459
Abhay Chaurasia <118441634+abhay2321@users.noreply.github.com>
2024-07-13 23:05:56 +0530

    Delete GOVIND PORTFOLIO directory

--- GOVIND PORTFOLIO/script.js
+++
@@ -1,8 +0,0 @@
-
-        var typed = new Typed('#element', {
-            strings: ['Web Developer', 'Problem solver', 'Web designer', 'Video Editor'],
-            typeSpeed: 50,
-            backSpeed: 50,
-            loop: true
-        });
-    
